6a9cc74f627f37e8d11d49133d6d5ef59beb5fd6
Siddharth Khandelwal <roadrash.sk@gmail.com>
2022-05-03 15:18:22 +0530

    Init

---
+++ exchangegram_frontend/src/components/Feed.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import { MasonryLayout, Spinner } from './index';
+import { searchQuery, feedQuery } from '../utils/data';
+
+function Feed() {
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
+
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client
+        .fetch(query)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    } else {
+      client
+        .fetch(feedQuery)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    }
+  }, [categoryId]);
+
+  if (loading) {
+    return <Spinner message="We are adding new ideas to your feed!" />;
+  }
+
+  if (!pins?.length) {
+    return <h3>No posts available</h3>;
+  }
+
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
+}
+
+export default Feed;
---
+++ exchangegram_frontend/src/container/Home.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { SideBar, UserProfile } from '../components';
+import Pins from './Pins';
+import { userQuery } from '../utils/data';
+import { client } from '../client';
+import { fetchUser } from '../utils/fetchUser';
+import logo from '../assets/logo.png';
+
+const Home = () => {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const scrollRef = useRef(null);
+
+  const userInfo = fetchUser();
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.googleId);
+    client
+      .fetch(query)
+      .then((data) => {
+        setUser(data[0]);
+      })
+      .catch((err) => console.log(err));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
+        <SideBar user={user && user} />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+          <HiMenu
+            fontSize={40}
+            className="cursor-pointer"
+            onClick={() => setToggleSidebar(true)}
+          />
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="logo" className="w-28" />
+          </Link>
+        </div>
+        {toggleSidebar && (
+          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+            <div className="absolute w-full flex justify-end items-center p-2">
+              <AiFillCloseCircle
+                fontSize={30}
+                className="cursor-pointer"
+                onClick={() => setToggleSidebar(false)}
+              />
+            </div>
+            <SideBar user={user && user} closeToggle={setToggleSidebar} />
+          </div>
+        )}
+      </div>
+      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <Routes>
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+          <Route path="/*" element={<Pins user={user && user} />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
